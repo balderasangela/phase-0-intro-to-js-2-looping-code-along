d0aca1eeb6b82ee6514ad0267302b69a1edfe087
Angela <balderasangela@gmail.com>
2022-10-01 19:23:09 -0400

    COMPLETED ASSIGNMENT

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+const gifts = ["teddy bear","drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me`);
+}
+
+const names = ["Angela","Peach", "Fukia"];
+const event = ["birthday"]
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]} for the wonderful birthday gift!`);
+    }
+    return names;
+}
+writeCards(names);
+
+var i = 10; // begin at 10
+while (i > 0) {
+    console.log( i-- ); // prints 10, 9, ..., 1
+}
+console.log("Blast Off!");
